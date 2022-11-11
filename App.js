d74038e192a923540c67b4b2d0cdb1187f142af2
Jonas Dhein <jonasdhein@gmail.com>
2022-11-11 17:43:05 -0300

    Create expo app - templates

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import ViewEffect from "./src/screens/ViewEffect";
+import ViewState from "./src/screens/ViewState";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ViewEffect />
+
+      <StatusBar
+        translucent={false}
+        backgroundColor="#fff"
+        style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
