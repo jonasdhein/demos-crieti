6143c13b64e236328364565b7179cc3215d0fccf
Jonas Dhein <jonasdhein@gmail.com>
2022-11-18 14:50:23 -0300

    Task list with done

--- src/screens/ViewTasks.js
+++ src/screens/ViewTasks.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import CustomButton from '../components/CustomButton';
+import LottieView from 'lottie-react-native';
+import Checkbox from 'expo-checkbox';
 
 const ViewTasks = () => {
 
@@ -11,7 +13,8 @@ const ViewTasks = () => {
         if (task) {
             const newTask = {
                 id: String(new Date().getTime()),
-                name: task
+                name: task,
+                done: false
             }
 
             setTaskList([...taskList, newTask])
@@ -23,7 +26,32 @@ const ViewTasks = () => {
 
     const deleteTask = (id) => {
         //queremos excluir o item que contém este ID da lista
-        setTaskList([...taskList.filter((item) => item.id !== id)])
+        Alert.alert('Atenção', 'Deseja mesmo excluir a tarefa?', [
+            {
+                text: "Sim",
+                onPress: () => {
+                    setTaskList([...taskList.filter((item) => item.id !== id)])
+                }
+            },
+            {
+                text: "Não",
+                onPress: () => { }
+            }
+        ]);
+    }
+
+    const handleCheckTask = (id) => {
+
+        const newTaskList = taskList.map(item => {
+            if(item.id == id){
+                //encontramos o elemento a ser alterado
+                return {...item, done: !item.done}
+            }
+            return item;
+        })
+
+        setTaskList(newTaskList);
+
     }
 
     return (
@@ -45,18 +73,35 @@ const ViewTasks = () => {
                 label="Salvar"
                 onPress={updateTaskList}
             />
-
             {
-                taskList.map((item) => {
-                    return (
-                        <TouchableOpacity
-                            style={{ margin: 4 }}
-                            key={item.id}
-                            onLongPress={() => deleteTask(item.id)}>
-                            <Text style={{ color: '#fff', fontSize: 24 }}>{item.name}</Text>
-                        </TouchableOpacity>
-                    )
-                })
+                taskList.length > 0 ?
+                    taskList.map(item => {
+                        return (
+                            <View
+                                key={item.id}
+                                style={styles.itemList}>
+                                <Checkbox
+                                    value={item.done}
+                                    onValueChange={() => handleCheckTask(item.id)}
+                                    color={item.done ? '#444' : '#fff'}
+                                />
+                                <Text style={[styles.itemText, { textDecorationLine: item.done ? 'line-through' : 'none' }]}>{item.name}</Text>
+                            </View>
+                        )
+                    })
+                    :
+                    <View style={{ flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                        <LottieView
+                            autoPlay
+                            loop={true}
+                            style={{
+                                width: 250,
+                                height: 250,
+                            }}
+                            source={require('../assets/animations/empty.json')}
+                        />
+                        <Text style={{ color: '#bebebe', fontSize: 20 }}>Lista vazia</Text>
+                    </View>
             }
         </View>
     );
@@ -77,10 +122,21 @@ const styles = StyleSheet.create({
         borderBottomColor: '#fff',
         borderBottomWidth: 1,
         color: '#fff',
-        fontSize: 14
+        fontSize: 16
     },
     label: {
         color: '#fff',
-        fontSize: 18
+        fontSize: 20
+    },
+    itemList: {
+        width: '100%',
+        marginTop: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    itemText: {
+        color: '#fff',
+        fontSize: 24
     }
 });
